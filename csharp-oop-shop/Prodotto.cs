4e4a9c74293e100661f8820554fbe4dee9d9a511
Federico Ceteroni <developer@overside.it>
2023-09-18 17:21:47 +0200

    complete all exercise with bonus

---
+++ csharp-oop-shop/Prodotto.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace csharp_oop_shop
+{
+    class Prodotto
+    {
+        private string code;
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        private decimal price;
+        public string Iva { get; set; }
+
+        public Prodotto(string name, string description, decimal price, string iva)
+        {
+            int cod = new Random().Next(1000, 9999);
+            this.code = cod.ToString();
+            this.Name = name;
+            this.Description = description;
+            this.price = price;
+            this.Iva = iva;
+        }
+
+        // ottenere il prezzo base
+        public decimal GetPrezzoBase()
+        {
+            return price;
+        }
+        public string GetCodePad()
+        {
+            
+            return code.PadLeft(8,'0');
+        }
+
+        // ottenere il prezzo comprensivo di IVA
+        public decimal GetPrezzoConIva(string iva)
+        {
+            if (decimal.TryParse(iva, out decimal ivaValue))
+            {
+                return price + (price * (ivaValue / 100));
+            }
+            else
+            {
+                throw new Exception("Iva non valida");
+            }
+        }
+
+        // Metodo per ottenere il nome esteso (codice + nome)
+        public string GetNomeEsteso()
+        {
+            return $"{code} {Name}";
+        }
+
+        public override string ToString() //serve per sovrascrivere il metodo Console.WriteLine()
+        {
+            return $"Codice: {GetCodePad()}, Nome: {Name}, Descrizione: {Description}, Prezzo: {price}, IVA: {Iva}";
+        }
+
+      
+    }
+}
--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using csharp_oop_shop;
+using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace csharpoopshop
 {
@@ -7,6 +9,47 @@ namespace csharpoopshop
         static void Main(string[] args)
         {
             Console.WriteLine("ciao");
+
+            Prodotto gelato = new Prodotto("Gelato al limone", "Buono gelato al limone", 25.7m, "22");
+            Console.WriteLine(gelato);
+            Console.WriteLine(gelato.GetCodePad());
+
+            Console.WriteLine("Inserisci iva");
+
+            gelato.GetPrezzoConIva(Console.ReadLine());
+            Prodotto[] negozio = new Prodotto[5];
+
+            negozio[0] = new Prodotto("Gelato al cioccolato", "Delizioso gelato al cioccolato", 3.5m, "22");
+            negozio[1] = new Prodotto("Maglietta", "Maglietta di cotone", 15.0m, "18");
+            negozio[2] = new Prodotto("Smartphone", "Ultimo modello di smartphone", 699.99m, "22");
+            negozio[3] = new Prodotto("Libro", "Bestseller del momento", 12.99m, "4");
+            negozio[4] = new Prodotto("Scarpe da corsa", "Scarpe leggere per correre", 49.99m, "22");
+
+            foreach (var i in negozio)
+            {
+                Console.WriteLine(i);
+            }
+
         }
     }
 }
+
+//creare la classe Prodotto che gestisce i prodotti di uno shop.
+/*Un prodotto è caratterizzato da:
+codice(numero intero)
+nome
+descrizione
+prezzo
+iva
+Usate opportunamente i livelli di accesso (public, private), i costruttori, i metodi getter e setter ed eventuali altri metodi di “utilità” per fare in modo che:
+alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
+Il codice prodotto sia accessibile solo in lettura
+Gli altri attributi siano accessibili sia in lettura che in scrittura
+Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
+Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome
+Testate poi i vostri oggetti Prodotto, istanziandoli e provando ad interagirci per testare tutti i metodi che avete previsto.
+BONUS:
+create un metodo che restituisca il codice con un pad left di 0 per arrivare a 8 caratteri (ad esempio codice 91 diventa 00000091, mentre codice 12344556 resta come è)
+Usando un array fisso di 5 prodotti, dichiarate un elenco dei prodotti di un negozio e inseriteci al suo interno qualche prodotto che vi aspettate di trovare nel negozio. Stampate poi l’elenco dei vostri prodotti che avete previsto nel negozio.
+Buon lavoro!
+*/
